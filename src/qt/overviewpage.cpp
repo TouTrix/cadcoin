49bae1bf1f46faa4d02edb9543e5565a392b19ec
c3rv3au <c3rv3au@gmail.com>
2016-10-05 01:56:54 -0400

    First update

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -26,7 +26,7 @@ class TxViewDelegate : public QAbstractItemDelegate
     Q_OBJECT
 public:
     TxViewDelegate(const PlatformStyle *_platformStyle):
-        QAbstractItemDelegate(), unit(BitcoinUnits::BTC),
+        QAbstractItemDelegate(), unit(BitcoinUnits::CAD),
         platformStyle(_platformStyle)
     {
 
@@ -242,7 +242,7 @@ void OverviewPage::setWalletModel(WalletModel *model)
         connect(model, SIGNAL(notifyWatchonlyChanged(bool)), this, SLOT(updateWatchOnlyLabels(bool)));
     }
 
-    // update the display unit, to not use the default ("BTC")
+    // update the display unit, to not use the default ("CAD")
     updateDisplayUnit();
 }
 
